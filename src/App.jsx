3cdd94b81aaf6b7897bd5afd53305d8a009d32cb
Programming Hero <programming.hero1@gmail.com>
2023-05-21 17:48:42 -0500

    basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
