373808334ddacf70d24069536c66746c6a82e853
Programming Hero <programming.hero1@gmail.com>
2023-05-22 06:57:49 -0500

    added view full menu button

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -27,6 +27,7 @@ const PopularMenu = () => {
                     ></MenuItem>)
                 }
             </div>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
         </section>
     );
 };
