36c4362f78b899ddf91a2b07c3dba8779bb50564
Programming Hero <programming.hero1@gmail.com>
2023-05-21 22:38:52 -0500

    added category swiper

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import slide1 from '../../../assets/home/slide1.jpg';
+import slide2 from '../../../assets/home/slide2.jpg';
+import slide3 from '../../../assets/home/slide3.jpg';
+import slide4 from '../../../assets/home/slide4.jpg';
+import slide5 from '../../../assets/home/slide5.jpg';
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-24"
+        >
+            <SwiperSlide>
+                <img src={slide1} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-16 text-white">Pizzas</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-16 text-white">Soups</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-16 text-white">Desserts</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
