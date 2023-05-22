37a40362b5515cdf98340c123298bed71242e0ba
Programming Hero <programming.hero1@gmail.com>
2023-05-21 21:06:51 -0500

    added banner

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,36 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import img1 from '../../../assets/home/01.jpg';
+import img2 from '../../../assets/home/02.jpg';
+import img3 from '../../../assets/home/03.png';
+import img4 from '../../../assets/home/04.jpg';
+import img5 from '../../../assets/home/05.png';
+import img6 from '../../../assets/home/06.png';
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <div>
+                <img src={img1} />
+            </div>
+            <div>
+                <img src={img2} />
+            </div>
+            <div>
+                <img src={img3} />
+            </div>
+            <div>
+                <img src={img4} />
+            </div>
+            <div>
+                <img src={img5} />
+            </div>
+            <div>
+                <img src={img6} />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <Banner></Banner>
         </div>
     );
 };
