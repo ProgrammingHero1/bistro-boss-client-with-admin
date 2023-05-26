025d285415030c72135270dab7a4f427de87287b
Programming Hero <programming.hero1@gmail.com>
2023-05-25 22:03:32 -0500

    use tan stack query to load data

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,4 +1,18 @@
-const useCart = email => {
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+const useCart = () => {
+    const { user } = useContext(AuthContext);
+
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return res.json();
+        },
+    })
+
+    return [cart, refetch]
 
 }
 export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,22 @@ import {
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
