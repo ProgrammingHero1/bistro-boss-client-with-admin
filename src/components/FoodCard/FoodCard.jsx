970dbf51954cffb7b68188f0886778839b10cf59
Programming Hero <programming.hero1@gmail.com>
2023-05-25 21:18:36 -0500

    send add to order data to the server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,10 +1,45 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
     const handleAddToCart = item => {
         console.log(item);
-        // 
+        if(user){
+            fetch('http://localhost:5000/carts')
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please login to order the food',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login')
+                }
+              })
+        }
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
