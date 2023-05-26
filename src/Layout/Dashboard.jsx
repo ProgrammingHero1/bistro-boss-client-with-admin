75d5775a57317af7daddb4126a22145b5502eb10
Programming Hero <programming.hero1@gmail.com>
2023-05-26 00:02:18 -0500

    load my cart data and calculate total

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,27 +1,27 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
 
 const Dashboard = () => {
     return (
-        <div className="drawer drawer-mobile">
+        <div className="drawer drawer-mobile ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side bg-[#D1A054]">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                <ul className="menu p-4 w-80">
 
-                    <li><Link><FaHome></FaHome> User Home</Link></li>
-                    <li><Link><FaCalendarAlt></FaCalendarAlt> Reservations</Link></li>
-                    <li><Link><FaWallet></FaWallet> Payment History</Link></li>
-                    <li><Link><FaShoppingCart></FaShoppingCart> My Cart</Link></li>
+                    <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
+                    <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
+                    <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
+                    <li><NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
                     <div className="divider"></div>
-                    <li><Link to="/"><FaHome></FaHome> Home</Link> </li>
-                    <li><Link to="/menu"> Our Menu</Link></li>
-                    <li><Link to="/order/salad">Order Food</Link></li>
+                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
+                    <li><NavLink to="/menu"> Our Menu</NavLink></li>
+                    <li><NavLink to="/order/salad">Order Food</NavLink></li>
                 </ul>
 
             </div>
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,8 +1,20 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
 
 const MyCart = () => {
+    const [cart] = useCart();
+    console.log(cart);
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
         <div>
-            <h2>My Cart</h2>
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            <div className="uppercase">
+                <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">PAY</button>
+            </div>
         </div>
     );
 };
