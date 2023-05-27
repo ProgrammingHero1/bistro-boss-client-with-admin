97f8aff3e6695a15b12da78fa7ecde95cf6acf9b
Programming Hero <programming.hero1@gmail.com>
2023-05-26 23:05:04 -0500

    make user admin

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,4 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
@@ -7,9 +10,65 @@ const AllUsers = () => {
         return res.json();
     })
 
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleDelete = user => {
+
+    }
+
     return (
-        <div>
-            {users.length}
+        <div className="w-full">
+            <Helmet>
+                <title>Bistro Boss | All users</title>
+            </Helmet>
+            <h3 className="text-3xl font-semibold my-4">Total Users: {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{ user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600  text-white"><FaUserShield></FaUserShield></button> 
+                                    }</td>
+                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
