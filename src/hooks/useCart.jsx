2174c7fb3b26b344e592b3063a27841a187f42df
Programming Hero <programming.hero1@gmail.com>
2023-05-27 07:12:09 -0500

    send verification auth header to the server

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,11 +3,14 @@ import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 const useCart = () => {
     const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, { headers: {
+                authorization: `bearer ${token}`
+            }})
             return res.json();
         },
     })
